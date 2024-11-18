851ec108d8e6bb38b1d898d47afc677156f0a746
rambe98 <81672761+rambe98@users.noreply.github.com>
2024-11-18 17:34:59 +0900

    backend setup

---
+++ backend/java/project/project/src/main/java/controller/NongConteroller.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class NongConteroller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ backend/java/project/project/src/main/java/service/NongService.java
@@ -0,0 +1,10 @@
+package service;
+
+public class NongService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

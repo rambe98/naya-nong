eb71ba51135cc8d3ed279f265ea41a13fecaf770
yunseungbum <ibm15444@naver.com>
2024-11-20 19:35:54 +0900

    board config add
    config add

--- backend/project/src/main/java/com/test/project/controller/BoardController.java
+++ backend/project/src/main/java/com/test/project/controller/BoardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.project.dto.BoardDTO;
-import com.test.project.service.BoardService;
+import com.test.project.service.BoardService; 
 
 import lombok.RequiredArgsConstructor;
 

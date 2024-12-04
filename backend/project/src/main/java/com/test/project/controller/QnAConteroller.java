23ec79965a832e35b3e18c8ca4f82f0da38268f8
yunseungbum <ibm15444@naver.com>
2024-12-04 09:21:11 +0900

    Revert "commit"
    This reverts commit e69728d39f50b6ea35ea3107dfda0c242e152496.

---
+++ backend/project/src/main/java/com/test/project/controller/QnAConteroller.java
@@ -0,0 +1,45 @@
+package com.test.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.project.dto.QnADTO;
+import com.test.project.service.QnAService;
+
+@RestController
+@RequestMapping("/qna")
+public class QnAConteroller {
+
+	@Autowired
+	private QnAService service;
+
+	@GetMapping
+	public ResponseEntity<?> showAllQna() {
+		List<QnADTO> products = service.showAllQna();
+		return ResponseEntity.ok(products);
+	}// showAllusers end
+	
+
+	@GetMapping("/{qnaNum}")
+	public ResponseEntity<?> showQna(@PathVariable("qnaNum") int qnaNum) {
+		QnADTO qna = service.showQna(qnaNum);
+		return ResponseEntity.ok(qna);
+	}// showAllusers end
+	
+
+	@PostMapping
+	public ResponseEntity<?> addQna(@RequestBody QnADTO dto) {
+		QnADTO addqna = service.addQna(dto);
+		return ResponseEntity.ok().body(addqna);
+	}// adduser end
+
+}// class end

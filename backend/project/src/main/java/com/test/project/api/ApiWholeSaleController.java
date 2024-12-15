d4f8bab5900c1127cb6fd51eeeaba5bf1462ba99
rambe98 <81672761+rambe98@users.noreply.github.com>
2024-12-15 16:22:49 +0900

    setup backend

--- backend/project/src/main/java/com/test/project/api/ApiWholeSaleController.java
+++ backend/project/src/main/java/com/test/project/api/ApiWholeSaleController.java
@@ -2,12 +2,15 @@ package com.test.project.api;
 
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 
 @RestController
@@ -21,6 +24,11 @@ public class ApiWholeSaleController {
     @PostMapping("/price/all")
     public ResponseEntity<List<PriceDataDTO>> getAllRetailPriceInfo(@RequestBody PriceRequestDTO priceRequestDTO) {
         try {
+        	// p_countrycode 필드에서만 <br> 태그 처리
+            String processedCountryCode = Jsoup.clean(
+                    HtmlUtils.htmlUnescape(priceRequestDTO.getP_countrycode()),
+                    Safelist.none().addTags("br") // <br> 태그만 허용
+            );
             // ApiService 호출하여 전체 데이터 리스트 얻기
             List<PriceDataDTO> allPriceDataList = wholeSaleService.getAllWholesalePriceData(
                     priceRequestDTO.getP_startday(),
@@ -29,7 +37,7 @@ public class ApiWholeSaleController {
                     priceRequestDTO.getP_itemcode(),
                     priceRequestDTO.getP_kindcode(),
                     priceRequestDTO.getP_productrankcode(),
-                    priceRequestDTO.getP_countrycode(),
+                    processedCountryCode,
                     priceRequestDTO.getP_returntype()
             );
 

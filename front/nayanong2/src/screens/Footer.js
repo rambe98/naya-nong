8a6d8316761a0e18916f62e0e28d3930e93010cd
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-12-18 14:47:51 +0900

    add

--- front/nayanong2/src/screens/Footer.js
+++ front/nayanong2/src/screens/Footer.js
@@ -5,9 +5,7 @@ import logo from "../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <div className="footer-logo-container">
-        <img src={logo} alt="농업 정보 플랫폼 로고" className="footer-logo" />
-      </div>
+      <img src={logo} alt="농업 정보 플랫폼 로고" className="footer-logo" />
       <div className="footer-content">
         <p>© 2024 농업 정보 플랫폼 - All Rights Reserved.</p>
         <p>

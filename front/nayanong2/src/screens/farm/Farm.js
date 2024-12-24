4b2f2be77902e4a9447a0056b828a1e4f1c2c0cf
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-12-24 11:06:01 +0900

    add

--- front/nayanong2/src/screens/farm/Farm.js
+++ front/nayanong2/src/screens/farm/Farm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "../../css/Farm.css";
 import { FaSearch } from "react-icons/fa";
 import axios from "axios";
 import { useRecoilState, useRecoilValue } from "recoil";

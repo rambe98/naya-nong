06b356b7cad11fe66cd43998ddb5bac6538caf23
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-12-24 15:43:36 +0900

    add

--- backend/project/src/main/java/com/test/project/config/SecurityConfig.java
+++ backend/project/src/main/java/com/test/project/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         .cors() // CORS 활성화
         .and()
             .authorizeHttpRequests()
-            .requestMatchers("/users/signin", "/users/signup","/users/**","/board", "/board/*", "/heart/**","/retail/**","/wholeSale/**","/qna").permitAll()
+            .requestMatchers("/users/signin", "/users/signup","/users/**","/board", "/board/*", "/heart/**","/retail/**","/wholeSale/**","/qna", "/board/search/**").permitAll()
             .requestMatchers("/comments/{bodNum}","/pComment/{comId}","/heart/{bodNum}/likeCount").permitAll()// 로그인, 회원가입은 모두 허용
             .anyRequest().authenticated();
          
